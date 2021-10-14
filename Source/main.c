7a6866a4f1de592d2b896721a14bf5cac0514231
Antoine A <59095838+Piazo@users.noreply.github.com>
2021-10-14 12:19:21 +0200

    Add files via upload

--- Source/main.c
+++ Source/main.c
@@ -1,6 +1,7 @@
 #include "stm32f10x.h"
 #include "MyTimer.h"
 #include "Driver_GPIO.h"
+#include "Acquisition_Girouette.h"
 
 void CallBack(){
 	My_GPIO_Toggle(GPIOC, 10);
@@ -8,20 +9,24 @@ void CallBack(){
 
 int main(void){
 	
-	/* Initialization of GPIOC 13 as Ouput OpenDrain */
+	setTimerEncoderMode(TIM2);
+	
+	
+	
+	/* Initialization of GPIOC 13 as Ouput OpenDrain *
 	MyGPIO_Struct gp = {GPIOA, 0, AltOut_Ppull};
 	MyGPIO_Init(&gp);
 	
 	/* Initialization of Timer1 with period 500ms invoking function Callback /
 	MyTimer_Base_Init (TIM1,36000,1000);
 	MyTimer_Base_Start (TIM1);
-	MyTimer_ActiveIT(TIM1, 1, CallBack);*/
+	MyTimer_ActiveIT(TIM1, 1, CallBack);*
 	
-	/* Tests of PWM */
+	/* Tests of PWM *
 	MyTimer_Base_Init (TIM2,36000,1000);
 	MyTimer_PWM(TIM2, 1);
 	MyTimer_PWM_StartPWM(TIM2);
-	MyTimer_PWM_SetDC(TIM2, 1, 40);
+	MyTimer_PWM_SetDC(TIM2, 1, 40);*/
 	
 	while(1) {
 		//if (GPIOC->IDR[]){
