c2dd2b303c1b909bf8f7b5644bd228fd9012b595
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2021-10-18 14:45:53 +0200

    [feat] intégration

--- Source/main.c
+++ Source/main.c
@@ -2,14 +2,10 @@
 #include "set_Sail.h"
 #include "stm32f10x.h"
 
-int sailAngleGLOBAL;
 
 int main(void) {
 
-    setTimerEncoderMode(TIM2);
+    setTimerEncoderMode();
+    configGir(GPIOA, 8);
 
-    int windAngle = acqGir_get_angle(260);
-
-    sailAngleGLOBAL = sSail_calc_angle(windAngle);
-    sSail_set_servo(sailAngleGLOBAL);
-
+}
