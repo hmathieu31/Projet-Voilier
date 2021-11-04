d22f89ab07eb0824f18a4ce814dbae6805670456
Antoine A <59095838+Piazo@users.noreply.github.com>
2021-11-04 11:21:23 +0100

    correction a revoir peut etre mdr

--- Source/Acquisition_Girouette.c
+++ Source/Acquisition_Girouette.c
@@ -2,28 +2,26 @@
 #include "acquisition_Girouette.h"
 #include "set_Sail.h"
 #include "MyTimer.h"
+#include "Driver_GPIO.h"
 
 int angleGLOBAL;
 
 
 void acqGir_set_timer_encoderMode() {
-
+	MyTimer_Base_Init(TIMER_ACQ, 360,0);
 	//On active le mode encoder qui permet de transformer le timer en compteur
-	TIMER_ACQ->SMCR &= ~(1<<2);
 	TIMER_ACQ->SMCR |= 3<<0;
 	//On gere le registre CCER pour que les signaux soient captures sur un front montant
-	TIMER_ACQ->CCER |= 1<<0;
-	TIMER_ACQ->CCER |= 1<<4;
+	TIMER_ACQ->CCER &= ~(0xAA); //TI1 and TI2 polarity
 	//On enable le counter
 	TIMER_ACQ->CR1 |= 1<<0;
 	
-	TIM2->CCMR1 &= ~(0xf<<4);
-	TIM2->CCMR1 &= ~(0xf<<12); //Compte tous les fronts
-	TIM2->CCMR1 |= 2; // T1FP1 mapped on TI1
-	TIM2->CCMR1 |= (2<<8); // T2FP2 mapped on TI2
+	TIMER_ACQ->CCMR1 &= ~(0xf<<4);
+	TIMER_ACQ->CCMR1 &= ~(0xf<<12); //Compte tous les fronts
+	TIMER_ACQ->CCMR1 |= 2; // T1FP1 mapped on TI1
+	TIMER_ACQ->CCMR1 |= (2<<8); // T2FP2 mapped on TI2
 	
-	TIMER_ACQ->PSC = 0;
-	TIMER_ACQ->ARR = 360;
+	
 	
 	TIMER_ACQ->CNT=0;
 }
@@ -39,8 +37,10 @@ void updateAngle(){
 }
 
 void acqGir_config_Gir(GPIO_TypeDef * GPIO, char pin){
-	while ((GPIO->IDR >> pin) &1 == 0){
-	}
+	MyGPIO_Struct gpiostruct = {GPIO, pin, In_PullUp};
+	MyGPIO_Init(&gpiostruct);
+	while (!MyGPIO_Read(GPIO, pin)){}
+	MyTimer_Base_Start(TIMER_ACQ);
 	TIMER_ACQ->CNT = 0;
 	angleGLOBAL = 0;
 }
