c8fe9b7139570fb9c678fab4827bc7f39aaec475
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2021-11-15 11:13:21 +0100

    Some more refactoring

--- Include/Usart-Driver.h
+++ Include/Usart-Driver.h
@@ -6,6 +6,8 @@
 void MyUSART_Init (USART_TypeDef * Usart);
 void MyUSART_Send (USART_TypeDef * Usart, char Data);
 void USART1_IRQHandler(void);
-void TournerPlateau(void);
+void TournerPlateau(signed char recu);
+
+void CapControl_start(void);
 
 #endif
--- Source/main.c
+++ Source/main.c
@@ -8,37 +8,13 @@
 
 int main(void){
 
-	
-	MyGPIO_Init( GPIOA , 10,  In_Floating ); // Correspond à l'USART
-
-	MyGPIO_Init(GPIOB, 6, AltOut_Ppull); // Correspond à la PWM
-	MyGPIO_Init(GPIOC, 7, Out_Ppull); // Correspond au bit de direction
-
-	
-	MyTimer_Base_Init(TIM4, 9999 , 3599); //ARR et PSC
-
-	
-	Init_MyTimer_PWM(TIM4,1);
-	MyTimer_Base_Start(TIM4);
-	
-	MyUSART_Init(USART1);
+	CapControl_start();
 	
 
 	
-	
-	/*
-	//Appeler PWM avec comme argument rapport (qui est en valeur absolue)
-	MyTimer_Base_Start(TIM4);
-	Set_Duty_PWM(TIM4,1,80); //modulo 100 */
 	
 	
 	while(1) {
-		//if (GPIOC->IDR[]){
-			//GPIOC->ODR |= (0x01 << 12);
-		//}
-		//Set_Duty_PWM(TIM1,1,pourcent);
-		
-		//TournerPlateau();
 	}
 	
 }
